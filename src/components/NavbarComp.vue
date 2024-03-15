8e0e44794a4a775e336fab7be48b7d3d83f20e7b
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-03-15 22:08:04 +0500

    feat: add basic layout

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,6 +1,29 @@
 <template>
-  <LogoComp />
-  <div>Navbar</div>
+  <nav class="navbar">
+    <div class="navbar-left">
+      <LogoComp />
+    </div>
+
+    <div class="navbar-middle">
+      <div class="navbar-menu">
+        <ul class="navbar-list">
+          <li class="navbar-item">
+            <a class="navbar-link" href="#">Furniture</a>
+          </li>
+
+          <li class="navbar-item">
+            <a class="navbar-link" href="#">About us</a>
+          </li>
+
+          <li class="navbar-item">
+            <a class="navbar-link" href="#">Contact us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="navbar-right"></div>
+  </nav>
 </template>
 
 <script>

fdc70efaf4a2836398d7538c6007c83aaebd218a
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-03-17 18:58:00 +0500

    feat: import google fonts

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,9 @@ export default {
 </script>
 
 <style>
+/*==================== GOOGLE FONTS ====================*/
+@import url("https://fonts.googleapis.com/css2?family=Athiti:wght@200;300;400;500;600;700&family=Flow+Block&family=Ojuju:wght@200..800&display=swap");
+
 /*==================== BASE ====================*/
 * {
   box-sizing: border-box;

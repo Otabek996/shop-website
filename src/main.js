f185e1ee1bb2ead08f881f9cba9a1e4676680b52
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-03-19 14:26:48 +0500

    feat: install remixicon

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// Remix icon
+import "remixicon/fonts/remixicon.css";
+
 createApp(App).use(router).mount("#app");
